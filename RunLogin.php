4b754d15f160f03af273070607a141426b59139f
Lucy <TunnelBlanket@users.noreply.github.com>
2015-01-24 14:09:59 -0800

    Updated with an example

--- RunLogin.php
+++ RunLogin.php
@@ -2,6 +2,8 @@
 
 namespace Kitsune\ClubPenguin;
 
+date_default_timezone_set("America/Los_Angeles");
+
 error_reporting(E_ALL ^ E_STRICT);
 
 spl_autoload_register(function ($path) {
@@ -14,7 +16,13 @@ spl_autoload_register(function ($path) {
 });
 
 $cp = new Login();
+
+// A simple example of binding to multiple ports and/or addresses
+// $cp->listen(["127.0.0.1", "192.168.1.159"], [6112, 7432])
+// $cp->listen(["127.0.0.1"], [6112, 7432])
+
 $cp->listen(0, 6112);
+
 while(true) {
 	$cp->acceptClients();
 }
