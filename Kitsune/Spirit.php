c435667ce55f3adb2690bc5d3bd8a81a3a9bf2f0
Lucy <TunnelBlanket@users.noreply.github.com>
2015-01-24 14:09:44 -0800

    Can now listen on multiple addresses

--- Kitsune/Spirit.php
+++ Kitsune/Spirit.php
@@ -2,6 +2,8 @@
 
 namespace Kitsune;
 
+class BindException extends \Exception {}
+
 abstract class Spirit {
 
 	protected $sockets = array();
@@ -35,11 +37,76 @@ abstract class Spirit {
 		socket_close($socket);
 	}
 
-	public function listen($address, $port, $backlog = 5) {
+	public function listen($address, $port, $backlog = 5, $throwException = false) {
 		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
 		socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
 		socket_set_nonblock($socket);
-		socket_bind($socket, $address, $port);
+
+		// Binding to a single port
+		if(!is_array($address) && !is_array($port)) {
+			$success = socket_bind($socket, $address, $port);
+		} else {
+			// Binding to multiple addresses/ports
+
+			if(is_array($address)) {
+				foreach($address as $_address) {
+					if(is_array($port)) {
+						/*
+						We also need to bind to multiple ports so lets
+						also get that out of the way.
+						*/
+
+						foreach($port as $_port) {
+							$success = socket_bind($socket, $_address, $_port);
+
+							/* 
+							We failed to bind to a port, so we're exiting
+							in order to throw an exception.
+							*/
+							if($success !== true) {
+								break 2;
+							}
+						}
+
+					} else {
+						$success = socket_bind($socket, $_address, $port);
+
+						if($success !== true) {
+							break;
+						}
+					}
+				}
+			} else {
+				/* 
+				We're not binding to multiple addresses, but we are
+				binding to multiple ports.
+				*/
+
+				foreach($port as $_port) {
+					$success = socket_bind($socket, $address, $_port);
+
+					if($success !== true) {
+						break;
+					}
+				}
+			}
+
+		}
+
+		if($success === false) {
+			// I know this is ugly, but I didn't want to make the lines too long
+			
+			if($throwException !== false){
+				throw new BindException(
+					"Error binding to port $port: " .
+					socket_strerror(socket_last_error($socket))
+				);
+			} else {
+				return false;
+			}
+		}
+
 		socket_listen($socket, $backlog);
 
 		$this->masterSocket = $socket;
